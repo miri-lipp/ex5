6917aa3af91dd83b06bd8e0ad551777cdabc1716
Miriam <miryam.lipkovich@gmail.com>
2025-01-01 15:30:10 +0200

    some small changes and a bit of optimisation

--- ex5.c
+++ ex5.c
@@ -1,6 +1,6 @@
 /******************
-Name: Miriam Lipkovich
-ID:336239652
+Name:
+ID:
 Assignment: ex5
 *******************/
 #include <stdio.h>
@@ -204,7 +204,7 @@ char *ReadLine() { //function to read new line  every time updating it through r
     char ch;
     while (1) { //i WILL do it with recursion i swear
         scanf("%c", &ch);
-        if (ch == '\n') //to not input \n in my string because printf goes brrr if i do and in windows that it won't read return
+        if (ch == '\n' || ch == 'r') //to not input \n in my string because printf goes brrr if i do and in windows that it won't read return
             break;
         if (length >= capacity) { //if length bigger than capacity then reallocate buffer
             capacity *= 2;
